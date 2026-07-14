ef089679409421e87731f7032c5f341392294396
Apoorv Mehrotra <apoorv.mehrotra.am@gmail.com>
2026-07-14 23:31:47 +0530

    Add: sql-apples-and-oranges - submission-0

---
+++ SQL/sql-apples-and-oranges/submission-0.sql
@@ -0,0 +1,8 @@
+SELECT
+    sale_date,
+    (SUM(CASE WHEN fruit = 'apples' THEN sold_num ELSE 0 END)-
+    SUM(CASE WHEN fruit = 'oranges' THEN sold_num ELSE 0 END)
+    ) as diff
+    FROM sales
+GROUP BY sale_date
+ORDER BY sale_date;
